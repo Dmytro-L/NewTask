ff765cc39fc12bcb8a49e187114130043f3c6315
Dmytro-L <67592803+Dmytro-L@users.noreply.github.com>
2021-03-05 17:41:03 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,58 @@
+
+//Меню mobile
+
+$(document).ready(function() {
+    $('.burger').click(function(event) {
+        $('.burger').toggleClass('active');
+        $('.menu').toggleClass('action');
+        $('body').toggleClass('lock');
+        
+    });
+});
+
+new Swiper('.portfolio__slider', {
+    slidesPerView: 1,
+    //Стрелки
+    navigation: {
+        nextEl: '.next',
+        prevEl: '.prev'
+    },
+
+    //Буллеты
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    //Превью
+    thumbs: {
+        swiper: {
+            el: '.mini-slider',
+            slidesPerView: 4,
+        }
+    },
+});
+
+new Swiper('.comment__swiper', {
+    navigation: {
+        nextEl: '.next-arr',
+        prevEl: '.prev-arr'
+    },
+    //Буллеты
+
+    pagination: {
+        el: '.pagination',
+        clickable: true,
+    },
+    slidesPerView: 1,
+});
+
+
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if(height > 0){
+    $('.header__main').css({'background-color' : 'rgba(94, 93, 93, 0.37)'});
+    } else{
+    $('.header__main').css({'background-color' : 'transparent'});
+    }
+});
